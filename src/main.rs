3624470ccb26f75e975899c3672166555810fab4
Neelzee <nilsien2001@gmail.com>
2023-10-15 23:02:05 +0200

    started on a level controller

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use bevy::prelude::*;
 use bevy_ecs_ldtk::prelude::*;
 use bevy_rapier2d::prelude::*;
 
+mod level_controller;
+
 fn main() {
 
 }
---
+++ src/utils/mod.rs
@@ -0,0 +1,5 @@
+pub mod constants {
+    pub const LEVEL_IDS: [&str] = [
+
+    ];
+}
