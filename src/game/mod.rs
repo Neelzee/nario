7c035b618b5d7cfa917fe856d5cc7d29c9332568
Neelzee <nilsien2001@gmail.com>
2023-10-15 22:01:47 +0200

    removed bad code

--- src/game/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod level_controller;
-
-pub mod camera_plugin;
-
-pub mod player;
--- src/utils/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod Const {
-    pub const LEVEL_IDS: [&str; 2] = [
-        "defb6e80-6280-11ee-b97c-71dea0997806",
-        "9ba5d230-6280-11ee-af3d-1b02cfdf6442"
-    ];
-}
